f8174f046d9ad20fc9b33ac1b2f7a6a49b78c3cf
monsterlsh <monsterlsh@163.com>
2021-06-26 10:31:46 +0800

    uva12108

---
+++ Purple-book-liurujia2th/ch4/4-15-uva12108.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#define RE
+int main()
+{
+#ifdef RE
+    freopen("input.in", "r", stdin);
+    freopen("output.out", "w", stdout);
+#endif
+cout<<string(2,'0');
+}
