8597b97b0c4173f6ba3b13eb3dddc35d0e7b5f8c
monsterlsh <monsterlsh@163.com>
2021-06-28 14:17:04 +0800

    it maybe accepted

--- Purple-book-liurujia2th/ch5/5-4-uva156.cpp
+++ Purple-book-liurujia2th/ch5/5-4-uva156.cpp
@@ -13,17 +13,17 @@ int main(){
     #endif
     vector<size_t> nums(54,0);
     string str;
-    map<vector<size_t>,string> res;
+    map<vector<size_t>,pair<string,bool>> res;
     while(cin>>str && str!="#"){
         fill(nums.begin(),nums.begin()+54,0);
         for(size_t i=0;i<str.size();nums[str[i]-(islower(str[i])?'a':'A')]++,i++);
         auto it =res.find(nums);
         //cout<<"size:"<<res.size()<<endl;
-        if(it!=res.end())res.erase(it);
-        else res[nums]=str;
+        if(it!=res.end())res[nums].second=0;
+        else res[nums]=make_pair(str,1);
     }
     vector<string>out;
-    for(auto it=res.begin();it!=res.end();out.emplace_back(it->second),it++);
+    for(auto it=res.begin();it!=res.end();it++)if(it->second.second)out.emplace_back(it->second.first);
     sort(out.begin(),out.end());
     for(string str:out)cout<<str<<endl;
 }
